a9a7c478c427f536249d5ff61fbc1a91dd0090c9
Pil <70975556+arnaudpilato@users.noreply.github.com>
2020-11-22 19:23:18 +0100

    post challenge 03

---
+++ src/Controller/DefautController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller;
+
+use phpDocumentor\Reflection\DocBlock\Tags\Return_;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class DefautController extends AbstractController
+{
+    /**
+     * @Route("/app_index")
+     */
+    public function index() : Response
+    {
+        return $this->render('index.html.twig', [
+            'bienvenue' => 'Bienvenue sur Wild Series',
+        ]);
+    }
+}
