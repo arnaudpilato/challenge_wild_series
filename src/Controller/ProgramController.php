ce96338a60b316baac45351e96788fc1bb9abb8d
Pil <70975556+arnaudpilato@users.noreply.github.com>
2020-11-28 22:07:44 +0100

    finish chalenge 05

--- src/Controller/ProgramController.php
+++ src/Controller/ProgramController.php
@@ -7,6 +7,9 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ * @Route("/programs", name="program_")
+ */
 class ProgramController extends AbstractController
 {
     /**
@@ -16,6 +19,17 @@ class ProgramController extends AbstractController
     {
         return $this->render('program/index.html.twig', [
             'website' => 'Wild Séries',
-            ]);
+        ]);
+    }
+
+    /**
+     * @Route("/{page}", requirements={"page"="\d+"}, methods={"GET"}, name="show")
+     *
+     */
+    public function show($page): Response
+    {
+        return $this->render('program/show.html.twig', [
+            "page" => $page
+        ]);
     }
 }
